3fe57d69fc61fedb037a278a65b2dee64fcd1ded
Jadson Santos <cjadsonsantos@hotmail.com>
2024-01-21 20:27:14 -0300

    feature(Cotas.vue): create a component for cotas

---
+++ src/components/Cotas.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps(['message', 'value', 'selected']);
+</script>
+
+<template>
+  <div
+    class="bg-gray-300 rounded cursor-pointer opacity-80 text-center p-2"
+    :class="{ 'border-green-500': selected }"
+  >
+    <h1 class="font-normal text-2xl" v-text="'+' + value" />
+    <h2 class="font-thin" v-text="message" />
+  </div>
+</template>
+
+<style scoped>
+.border-green-500 {
+  border: 2px solid #48bb78;
+}
+</style>
