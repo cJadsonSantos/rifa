3daafb19a57858d6a8269f0cc46754574db52218
Jadson Santos <cjadsonsantos@hotmail.com>
2024-01-21 23:48:36 -0300

    feature(CountCotas.vue): add icons plus and minus from heroicons lib

--- src/components/CountCotas.vue
+++ src/components/CountCotas.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 import { useCounterStore } from '@/stores/counter'
+import { PlusCircleIcon } from '@heroicons/vue/24/outline'
+import { MinusCircleIcon } from '@heroicons/vue/24/outline'
 
 const store = useCounterStore()
 </script>
 
 <template>
   <div class="flex justify-center content-center items-center py-2 space-x-2">
-    <button @click="store.decrement" class="bg-red-500 text-white px-2 py-1 rounded">-</button>
+    <MinusCircleIcon @click="store.decrement" class="h-6 w-6 text-gray-400 cursor-pointer" />
     <input v-model="store.count" type="number" class="border px-2 py-1 text-center rounded-md" />
-    <button @click="store.increment" class="bg-green-500 text-white px-2 py-1 rounded">+</button>
+    <PlusCircleIcon @click="store.increment" class="h-6 w-6 text-gray-400 cursor-pointer" />
   </div>
 </template>
 
