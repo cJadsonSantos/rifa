18416f6d001d625507a802633df20318c2e66b29
Jadson Santos <cjadsonsantos@hotmail.com>
2024-01-21 22:48:08 -0300

    feature(CountCotas.vue): create a component for count cotas

---
+++ src/components/CountCotas.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+
+const store = useCounterStore()
+</script>
+
+<template>
+  <div class="flex justify-center content-center items-center space-x-2">
+    <button @click="store.decrement" class="bg-red-500 text-white px-2 py-1 rounded">-</button>
+    <input v-model="store.count" type="number" class="border px-2 py-1 text-center rounded" />
+    <button @click="store.increment" class="bg-green-500 text-white px-2 py-1 rounded">+</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
