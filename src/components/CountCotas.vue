4f0b3cb2572d9cc896d2be1ff24431a3c67532a9
Jadson Santos <cjadsonsantos@hotmail.com>
2024-01-21 23:37:33 -0300

    feature(CountCotas.vue): add rounded-md on input

--- src/components/CountCotas.vue
+++ src/components/CountCotas.vue
@@ -5,9 +5,9 @@ const store = useCounterStore()
 </script>
 
 <template>
-  <div class="flex justify-center content-center items-center space-x-2">
+  <div class="flex justify-center content-center items-center py-2 space-x-2">
     <button @click="store.decrement" class="bg-red-500 text-white px-2 py-1 rounded">-</button>
-    <input v-model="store.count" type="number" class="border px-2 py-1 text-center rounded" />
+    <input v-model="store.count" type="number" class="border px-2 py-1 text-center rounded-md" />
     <button @click="store.increment" class="bg-green-500 text-white px-2 py-1 rounded">+</button>
   </div>
 </template>
